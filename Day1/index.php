228359b83ba6c9985e2029e81eb9485b3ac04bb3
Kalingga Fadel Muhamad <64252009+kalinggafadelmuhamad@users.noreply.github.com>
2020-08-23 16:03:33 +0700

    Add files via upload

---
+++ Day1/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/custom.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/aos.css">
+    <title>Day1 | company Profile</title>
+</head>
+<body>
+    <?php include 'app/views/navbar.php' ?>
+    <?php include 'app/views/heroimage.php' ?>
+    <?php include 'app/views/service.php' ?>
+    <?php include 'app/views/project.php' ?>
+    <?php include 'app/views/footer.php'  ?>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.js"></script>
+    <script src="assets/js/aos.js"></script>
+    <script>
+    AOS.init();
+    </script>
+    
+</body>
+
+</html>
