198ef9a3e0f3da83ecafbf76f200adc384e1d6c9
Kalingga Fadel Muhamad <64252009+kalinggafadelmuhamad@users.noreply.github.com>
2020-08-23 15:42:03 +0700

    Add files via upload

---
+++ Day2/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/css/bootstrap.css">
+    <link rel="stylesheet" href="assets/css/custom.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/aos.css">
+    <title>Day2 | corona </title>
+</head>
+<body>
+
+    <?php include 'app/views/navbar.php'; ?>
+    <?php include 'app/views/grafik.php'; ?>
+    <?php include 'app/views/kasus.php'; ?>
+    <?php include 'app/views/global.php'; ?>
+    <?php include 'app/views/footer.php'; ?>
+    <?php include 'app/views/lisence.php'; ?>
+    
+    <script src="https://cdn.jsdelivr.net/npm/chart.js@2.8.0"></script>
+    <script src="assets/js/chart.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.js"></script>
+    <script src="assets/js/aos.js"></script>
+    <script src="assets/js/main.js"></script>
+    <script>
+    AOS.init();
+    </script>
+    
+</body>
+
+</html>
